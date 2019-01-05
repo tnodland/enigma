4dcabf69f108fc2c98171bfc0f307336fdbb5a69
Trevor Nodland <trevor.nodland@gmail.com>
2019-01-05 11:16:29 -0700

    Refined initialize and att_reader to contain default values for the a b c and d shifts

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,8 +1,16 @@
 class Enigma
 
-  attr_reader :letters 
+  attr_reader :letters,
+              :a_shift,
+              :b_shift,
+              :c_shift,
+              :d_shift
 
   def initialize
     @letters = ("a".."z").to_a << " "
+    @a_shift = 0
+    @b_shift = 0
+    @c_shift = 0
+    @d_shift = 0
   end
 end
