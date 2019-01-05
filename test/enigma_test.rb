99f099cac5278b19c6ab04a7575ab32d6a9bdb92
Trevor Nodland <trevor.nodland@gmail.com>
2019-01-05 11:08:56 -0700

    Added and tested attributes for the enigma class

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -9,4 +9,12 @@ class EnigmaTest < Minitest::Test
 
     assert_instance_of Enigma, enigma
   end
+
+  def test_it_has_attributes
+    enigma = Enigma.new
+
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+    assert_equal expected, enigma.letters
+  end
 end
