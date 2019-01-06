d68a5c12e8a3e92ac9afe3b5e76d2c09e15fbc90
Trevor Nodland <trevor.nodland@gmail.com>
2019-01-06 15:09:17 -0700

    Gave AlphabetSoup the attributes of enigma for future refactor)

--- lib/alphabet_soup.rb
+++ lib/alphabet_soup.rb
@@ -1,2 +1,16 @@
 class AlphabetSoup
+
+  attr_reader :letters,
+              :a_shift,
+              :b_shift,
+              :c_shift,
+              :d_shift
+
+  def initialize(a_shift, b_shift, c_shift, d_shift)
+    @letters =  ("a".."z").to_a << " "
+    @a_shift = a_shift
+    @b_shift = b_shift
+    @c_shift = c_shift
+    @d_shift = d_shift
+  end
 end
--- test/alphabet_soup_test.rb
+++ test/alphabet_soup_test.rb
@@ -5,8 +5,20 @@ require 'minitest/pride'
 
 class AlphabetSoupTest < Minitest::Test
   def test_it_exists
-    soup = AlphabetSoup.new
+    soup = AlphabetSoup.new(3, 27, 73, 20)
 
     assert_instance_of AlphabetSoup, soup
+  end
+
+  def test_it_has_attributes
+    soup = AlphabetSoup.new(3, 27, 73, 20)
+
+    expected = expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+    assert_equal expected, soup.letters
+    assert_equal 3, soup.a_shift
+    assert_equal 27, soup.b_shift
+    assert_equal 73, soup.c_shift
+    assert_equal 20, soup.d_shift
   end
 end
