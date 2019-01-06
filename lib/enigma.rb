36bc2b0cf2d1e8a9126e0cbb5eb25b997bfc6c1a
Trevor Nodland <trevor.nodland@gmail.com>
2019-01-06 11:54:17 -0700

    Wrote a very ugly method for encryption, it works but is in dire need of refactoring

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -39,18 +39,102 @@ class Enigma
     self.key_converter(key)
     self.date_shifter(date)
     message = message.downcase
-    encrypted_message = []
+    encrypted_message_array = []
     message_array = message.split ""
 
     loop do
+      temp_letter_array = []
+
       if message_array.length >= 4
         letter_shifted_by_a = message_array.shift
+          @letters.each do |letter|
+            if letter_shifted_by_a == letter
+              temp_letter_array = @letters.rotate(@letters.index(letter))
+            end
+          end
+        encrypted_message_array << temp_letter_array.rotate(@a_shift).first
+
         letter_shifted_by_b = message_array.shift
+          @letters.each do |letter|
+            if letter_shifted_by_b == letter
+              temp_letter_array = @letters.rotate(@letters.index(letter))
+            end
+          end
+        encrypted_message_array << temp_letter_array.rotate(@b_shift).first
+
         letter_shifted_by_c = message_array.shift
+          @letters.each do |letter|
+            if letter_shifted_by_c == letter
+              temp_letter_array = @letters.rotate(@letters.index(letter))
+            end
+          end
+        encrypted_message_array << temp_letter_array.rotate(@c_shift).first
+
         letter_shifted_by_d = message_array.shift
+          @letters.each do |letter|
+            if letter_shifted_by_d == letter
+              temp_letter_array = @letters.rotate(@letters.index(letter))
+            end
+          end
+        encrypted_message_array << temp_letter_array.rotate(@d_shift).first
 
 
+
+      elsif message_array.length == 3
+        letter_shifted_by_a = message_array.shift
+          @letters.each do |letter|
+            if letter_shifted_by_a == letter
+              temp_letter_array = @letters.rotate(@letters.index(letter))
+            end
+          end
+        encrypted_message_array << temp_letter_array.rotate(@a_shift).first
+
+        letter_shifted_by_b = message_array.shift
+          @letters.each do |letter|
+            if letter_shifted_by_b == letter
+              temp_letter_array = @letters.rotate(@letters.index(letter))
+            end
+          end
+        encrypted_message_array << temp_letter_array.rotate(@b_shift).first
+
+        letter_shifted_by_c = message_array.shift
+          @letters.each do |letter|
+            if letter_shifted_by_c == letter
+              temp_letter_array = @letters.rotate(@letters.index(letter))
+            end
+          end
+        encrypted_message_array << temp_letter_array.rotate(@c_shift).first
+
+      elsif message_array.length == 2
+        letter_shifted_by_a = message_array.shift
+          @letters.each do |letter|
+            if letter_shifted_by_a == letter
+              temp_letter_array = @letters.rotate(@letters.index(letter))
+            end
+          end
+        encrypted_message_array << temp_letter_array.rotate(@a_shift).first
+
+        letter_shifted_by_b = message_array.shift
+          @letters.each do |letter|
+            if letter_shifted_by_b == letter
+              temp_letter_array = @letters.rotate(@letters.index(letter))
+            end
+          end
+        encrypted_message_array << temp_letter_array.rotate(@b_shift).first
+
+      elsif message_array.length == 1
+        letter_shifted_by_a = message_array.shift
+          @letters.each do |letter|
+            if letter_shifted_by_a == letter
+              temp_letter_array = @letters.rotate(@letters.index(letter))
+            end
+          end
+        encrypted_message_array << temp_letter_array.rotate(@a_shift).first
+
+      else
+        break
       end
     end
+    encrypted_message = encrypted_message_array.join
   end
 end
