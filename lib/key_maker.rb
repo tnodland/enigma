de7c31ee125a977fe34b5f443c671bf2bc5e2ad8
Trevor Nodland <trevor.nodland@gmail.com>
2019-01-08 20:12:28 -0700

    Added key_converter method into the key_maker module

--- lib/key_maker.rb
+++ lib/key_maker.rb
@@ -16,5 +16,12 @@ module KeyMaker
     return key
   end
 
-  
+  def key_converter(key)
+    key_array = key.split ""
+
+    @a_shift = key_array[0].to_i * 10 + key_array[1].to_i
+    @b_shift = key_array[1].to_i * 10 + key_array[2].to_i
+    @c_shift = key_array[2].to_i * 10 + key_array[3].to_i
+    @d_shift = key_array[3].to_i * 10 + key_array[4].to_i
+  end
 end
